2e5c34f4b8cd7ce43d6bf3729e7f605cfec8852c
Анастасия <anastas.93@mail.ru>
2023-08-16 22:06:55 +0300

    24

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -61,7 +61,6 @@ class RecipeViewSet(ModelViewSet):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance,
-                                         data=request.data,
                                          partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
