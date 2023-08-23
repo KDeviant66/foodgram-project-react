54679a943f2689f028ef2cb124252a85f85e9002
Анастасия <anastas.93@mail.ru>
2023-08-23 14:10:33 +0300

    fix admin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,18 +1,55 @@
 from django.contrib import admin
 
-from . import models
+from .models import (Favorite, Ingredient, Recipe,
+                     RecipeIngredient, ShoppingCart,
+                     Tag)
 
 
-class RecipeIngredientInline(admin.TabularInline):
-    model = models.RecipeIngredient
-    extra = 1
-
-
-@admin.register(models.Recipe)
 class TagAdmin(admin.ModelAdmin):
-    inlines = (RecipeIngredientInline, )
+    list_display = ('id', 'name', 'color', 'slug')
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug',)
+    ordering = ('name',)
 
 
-@admin.register(models.Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'measurement_unit')
+    search_fields = ('name', 'measurement_unit')
+    list_filter = ('name',)
+    ordering = ('id',)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'count_favorites')
+    search_fields = ('username', 'email', 'first_name', 'last_name',)
+    list_filter = ('author', 'name', 'tags',)
+    ordering = ('name',)
+
+    def count_favorites(self, obj):
+        return obj.favorites.count()
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient', 'amount')
+    search_fields = ('recipe', 'ingredient',)
+    list_filter = ('recipe', 'ingredient',)
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user', 'recipe',)
+    list_filter = ('user', 'recipe',)
+
+
+class ShoppingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user', 'recipe',)
+    list_filter = ('user', 'recipe',)
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ShoppingCart, ShoppingAdmin)
